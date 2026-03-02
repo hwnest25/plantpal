689e5294c4633c3aa56b2cbbc5ab8be4522567fb
i767824 <hope.wilson@sap.com>
2026-03-02 22:34:36 +0100

    Initial commit

---
+++ src/test/java/com/hwnest25/plantpal/PlantpalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwnest25.plantpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantpalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
