1afbc33ebfcffdcbf410fbc02d1e7a86f9c07c23
i767824 <hope.wilson@sap.com>
2026-03-04 06:22:18 +0100

    Fix string capitalization issue.

--- src/main/java/com/hwnest25/plantpal/model/PlantGrowthSpeed.java
+++ src/main/java/com/hwnest25/plantpal/model/PlantGrowthSpeed.java
@@ -2,4 +2,9 @@ package com.hwnest25.plantpal.model;
 
 public enum PlantGrowthSpeed {
     FAST, MEDIUM, SLOW;
+
+     @Override
+    public String toString() {
+        return name().charAt(0) + name().substring(1).toLowerCase();
+    }
 }
